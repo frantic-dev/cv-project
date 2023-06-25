cc30c669fa695d97c5f9c3d74a3b23a9691a10cc
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-06-25 23:30:44 +0100

    turn App class component into a functional component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,51 @@
-import { Component } from "react";
 import "./styles/App.css";
 import EducationForm from "./components/EducationForm";
 import ExperienceForm from "./components/ExperienceForm";
 import GeneralForm from "./components/GeneralForm";
 import SkillsForm from "./components/SkillsForm";
 import Cv from "./components/Cv";
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // cv: {
-      fullName: "Stella Walker",
-      email: "example@example.com",
-      number: "5555555555",
-      summary:
-        "Dedicated professional with demonstrated skills in customer service, time management and trend tracking. Experience working in challenging environments to provide customer-focused resolutions and feedback. Willing to take on any task to support team and help the business succeed.",
-      skills:
-        "Positive and professional,Conflict resolution,Request management, reliability, client advocacy, critical thinking, issue troubleshooting",
-      education: [
-        {
-          schoolName: "University of Washington, Seattle, WA1",
-          studyTitle: "Bachelor of Arts - Communication",
-          studyDate: "2017-05",
-        },
-      ],
-      currentEducation: 0,
-      experience: [
-        {
-          companyName: "Alaska Airlines | Seattle, WA",
-          positionTitle: "Lead Customer Service Agent",
-          jobTasks:
-            "Met face-to-face with customers to handle issues escalated by associates,  Followed up with customers to share findings and offer solutions for ongoing inquiries, Avoided revenue losses with diligent attention to customers' needs and complaints",
-          startingDate: "2019-05",
-          endingDate: "2023-06",
-        },
-      ],
-      currentExperience: 0,
-      // }
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-  }
-  handleClick(e) {
+import { useState } from "react";
+
+function App() {
+  const [info, setInfo] = useState({
+    fullName: "Stella Walker",
+    email: "example@example.com",
+    number: "5555555555",
+    summary:
+      "Dedicated professional with demonstrated skills in customer service, time management and trend tracking. Experience working in challenging environments to provide customer-focused resolutions and feedback. Willing to take on any task to support team and help the business succeed.",
+    skills:
+      "Positive and professional,Conflict resolution,Request management, reliability, client advocacy, critical thinking, issue troubleshooting",
+    education: [
+      {
+        schoolName: "University of Washington, Seattle, WA1",
+        studyTitle: "Bachelor of Arts - Communication",
+        studyDate: "2017-05",
+      },
+    ],
+    currentEducation: 0,
+    experience: [
+      {
+        companyName: "Alaska Airlines | Seattle, WA",
+        positionTitle: "Lead Customer Service Agent",
+        jobTasks:
+          "Met face-to-face with customers to handle issues escalated by associates,  Followed up with customers to share findings and offer solutions for ongoing inquiries, Avoided revenue losses with diligent attention to customers' needs and complaints",
+        startingDate: "2019-05",
+        endingDate: "2023-06",
+      },
+    ],
+    currentExperience: 0,
+  });
+
+  function handleClick(e) {
     let propName = e.target.previousElementSibling.textContent;
-    let inputIds = Object.keys(this.state[propName][0]);
+    let inputIds = Object.keys(info[propName][0]);
     let newProps = {};
     let updatedIndex =
       "current" + propName.charAt(0).toUpperCase() + propName.slice(1);
     inputIds.forEach((prop) => (newProps = { ...newProps, [prop]: "" }));
     console.log(newProps);
-    this.setState((prev) => ({
+    setInfo((prev) => ({
+      ...prev,
       [propName]: [...prev[propName], newProps],
       [updatedIndex]: prev[propName].length,
     }));
@@ -60,88 +54,102 @@ class App extends Component {
       input.value = "";
     });
   }
-  handleDelete(e) {
+
+  function handleDelete(e) {
     e.preventDefault();
+
     let sectionName = e.target.className.split("-")[0];
     let currentIndex =
       "current" + sectionName.charAt(0).toUpperCase() + sectionName.slice(1);
     let deleteBtns = document.getElementsByClassName(e.target.className);
     let index = [...deleteBtns].indexOf(e.target);
-    let inputIds = Object.keys(this.state[sectionName][0]);
+    let inputIds = Object.keys(info[sectionName][0]);
     let resetProps = {};
+
     inputIds.forEach((prop) => (resetProps = { ...resetProps, [prop]: "" }));
 
-    this.setState((prevState) => {
-      if (prevState[sectionName].length === 1)
+    setInfo((prevInfo) => {
+      if (prevInfo[sectionName].length === 1)
         return {
+          ...prevInfo,
           [sectionName]: [resetProps],
         };
       else
         return {
-          [sectionName]: prevState[sectionName].filter(
-            (school) => prevState[sectionName].indexOf(school) !== index
+          ...prevInfo,
+          [sectionName]: prevInfo[sectionName].filter(
+            (school) => prevInfo[sectionName].indexOf(school) !== index
           ),
-          [currentIndex]: prevState[sectionName].length - 2,
+          [currentIndex]: prevInfo[sectionName].length - 2,
         };
     });
   }
-  handleEdit(e) {
+
+  function handleEdit(e) {
     e.preventDefault();
+
     let sectionName = e.target.className.split("-")[0];
     let currentIndex =
       "current" + sectionName.charAt(0).toUpperCase() + sectionName.slice(1);
     let editBtns = document.getElementsByClassName(e.target.className);
     let index = [...editBtns].indexOf(e.target);
-    this.setState({
+
+    setInfo((prevInfo) => ({
+      ...prevInfo,
       [currentIndex]: index,
-    });
+    }));
   }
-  handleChange(e, currentIndex) {
+
+  function handleChange(e, currentIndex) {
     let key = e.target.id;
-    let stateArray = e.target.parentElement.className;
-    if (stateArray === "education" || stateArray === "experience") {
-      let currentProp = this.state[stateArray][currentIndex];
-      this.setState((prevState) => ({
-        [stateArray]: prevState[stateArray].map((school) =>
+    console.log("key is", key);
+    let infoArray = e.target.parentElement.className;
+    console.log("infoArray is", infoArray);
+
+    if (infoArray === "education" || infoArray === "experience") {
+      let currentProp = info[infoArray][currentIndex];
+      console.log("currentProp is", currentProp);
+      setInfo((prevInfo) => ({
+        ...prevInfo,
+        [infoArray]: prevInfo[infoArray].map((school) =>
           school === currentProp ? { ...school, [key]: e.target.value } : school
         ),
       }));
-    } else this.setState({ [key]: e.target.value });
+    } else setInfo((prevInfo) => ({ ...prevInfo, [key]: e.target.value }));
+    console.log("info is", info);
   }
-  render() {
-    return (
-      <div>
-        <h1 className="title">Cv project</h1>
-        <form>
-          <GeneralForm
-            fullName={this.state.fullName}
-            email={this.state.email}
-            number={this.state.number}
-            summary={this.state.summary}
-            onChange={this.handleChange}
-          />
-          <SkillsForm skills={this.state.skills} onChange={this.handleChange} />
-          <EducationForm
-            education={this.state.education}
-            onChange={this.handleChange}
-            handleClick={this.handleClick}
-            handleDelete={this.handleDelete}
-            handleEdit={this.handleEdit}
-            currentEducation={this.state.currentEducation}
-          />
-          <ExperienceForm
-            experience={this.state.experience}
-            onChange={this.handleChange}
-            handleClick={this.handleClick}
-            currentExperience={this.state.currentExperience}
-            handleEdit={this.handleEdit}
-            handleDelete={this.handleDelete}
-          />
-        </form>
-        <Cv information={this.state} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1 className="title">Cv project</h1>
+      <form>
+        <GeneralForm
+          fullName={info.fullName}
+          email={info.email}
+          number={info.number}
+          summary={info.summary}
+          onChange={handleChange}
+        />
+        <SkillsForm skills={info.skills} onChange={handleChange} />
+        <EducationForm
+          education={info.education}
+          onChange={handleChange}
+          handleClick={handleClick}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          currentEducation={info.currentEducation}
+        />
+        <ExperienceForm
+          experience={info.experience}
+          onChange={handleChange}
+          handleClick={handleClick}
+          currentExperience={info.currentExperience}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
+      </form>
+      <Cv information={info} />
+    </div>
+  );
 }
 export default App;
 
